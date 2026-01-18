12056d40b940af80122b4a40a75b32a5303eb09e
Gourab Ghosh <ggourab217@gmail.com>
2026-01-18 20:39:45 +0530

    Update Centripetal Force Calculation.c

--- Centripetal Force Calculation/Centripetal Force Calculation.c
+++ Centripetal Force Calculation/Centripetal Force Calculation.c
@@ -10,3 +10,7 @@ printf("Enter velocity (km/h): ");
 
  printf("Enter radius (km): ");
     scanf("%f", &radius_km);
+
+ mass_kg = mass_g / 1000.0;
+    velocity_ms = velocity_kmh / 3.6;
+    radius_m = radius_km * 1000.0;
