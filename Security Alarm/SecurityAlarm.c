8848978f44d0c7cb2ecc050c560f6ad362d0ca2f
Gourab Ghosh <ggourab217@gmail.com>
2026-01-19 22:37:46 +0530

    Update SecurityAlarm.c

--- Security Alarm/SecurityAlarm.c
+++ Security Alarm/SecurityAlarm.c
@@ -8,5 +8,7 @@ scanf("%d", &start_hour);
     scanf("%d", &delay_hours);
     raw_sum = start_hour + delay_hours;
     final_hour = raw_sum % 24;
-    raw_sum = start_hour + delay_hours;
-    final_hour = raw_sum % 24;
+    printf("Alarm will ring at hour: %d\n", final_hour);
+
+    return 0;
+}
