99ac8fbfbd082e7db253b5141dba7ed49544b88e
Gourab Ghosh <ggourab217@gmail.com>
2026-01-26 20:22:44 +0530

    Update ElevatorLogicSystem.c

--- ElevatorLogicSystem.c
+++ ElevatorLogicSystem.c
@@ -17,3 +17,35 @@ else if (batteryMode && currentWeight > maxLimit / 2)
         printf("S\n");
 else
         printf("M\n");
+ return 0;
+}
+Output
+
+S
+10. Crypto Trade Execution
+#include <stdio.h>
+#include <stdbool.h>
+
+int main() {
+    double currentPrice, targetPrice, walletBalance;
+    int amount, volatility, hour;
+    bool isWhale;
+
+    printf("Enter currentPrice, targetPrice, balance, amount, volatility, hour, whale: ");
+    scanf("%lf %lf %lf %d %d %d %d", &currentPrice, &targetPrice,
+          &walletBalance, &amount, &volatility, &hour, &isWhale);
+
+    if (hour >= 0 && hour <= 4)
+        printf("0\n");
+    else if (walletBalance < amount * currentPrice)
+        printf("0\n");
+    else if (volatility > 8 && !isWhale)
+        printf("0\n");
+    else if (currentPrice < targetPrice)
+        printf("1\n");
+    else
+        printf("0\n");
+
+    return 0;
+}
+Output
