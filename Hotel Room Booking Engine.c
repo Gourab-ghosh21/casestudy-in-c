419f55c997e1a88fbd394e8e2eb07f482f79c092
Gourab Ghosh <ggourab217@gmail.com>
2026-01-26 20:16:24 +0530

    Update Hotel Room Booking Engine.c

--- Hotel Room Booking Engine.c
+++ Hotel Room Booking Engine.c
@@ -7,3 +7,5 @@ int main()
 printf("Enter type, guests, age, day: ");
     scanf("%d %d %d %d", &type, &guests, &age, &day);
  price = (type == 1) ? 1000 : 1800;
+ if (type == 1 && guests > 1 && age >= 5)
+        price += (guests - 1) * 500;
