425a729b0b108d3a7db00966d359d96d48a9590c
Gourab Ghosh <ggourab217@gmail.com>
2026-01-28 22:13:31 +0530

    Update Convert Seconds to H:M:S.c

--- Convert Seconds to H:M:S/Convert Seconds to H:M:S.c
+++ Convert Seconds to H:M:S/Convert Seconds to H:M:S.c
@@ -4,7 +4,7 @@ int total_seconds, hours, minutes, seconds, remaining;
 
     printf("Enter total seconds: ");
 scanf("%d", &total_seconds);
-
+ 
     hours = total_seconds / 3600;
 remaining = total_seconds % 3600;
     minutes = remaining / 60;
