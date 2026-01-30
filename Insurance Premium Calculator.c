51ef3cd2fdb607580689db3a048a7a21f2c4f21e
Gourab Ghosh <ggourab217@gmail.com>
2026-01-30 23:41:18 +0530

    Update Insurance Premium Calculator.c

--- Insurance Premium Calculator.c
+++ Insurance Premium Calculator.c
@@ -7,3 +7,4 @@ int main()
    printf("Enter carValue, accidents, age, airbags, abs: ");
  scanf("%lf %d %d %d %d", &carValue, &accidents, &age, &airbags, &abs);
  premium = carValue * 0.05;
+if (accidents == 0)
