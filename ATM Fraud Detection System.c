d140aae2549c8c37c8b032c6329eb915ba505810
Gourab Ghosh <ggourab217@gmail.com>
2026-01-31 22:31:25 +0530

    Update ATM Fraud Detection System.c

--- ATM Fraud Detection System.c
+++ ATM Fraud Detection System.c
@@ -6,3 +6,4 @@ int pinAttempts, locationCode, homeCode;
  double amount, dailyTotal;
 bool isHighTrustUser;
  printf("Enter pinAttempts, amount, dailyTotal, trust, location, home: ");
+scanf("%d %lf %lf %d %d %d", &pinAttempts, &amount,
