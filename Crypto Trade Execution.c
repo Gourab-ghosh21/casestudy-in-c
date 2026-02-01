b0df3280c7ae5fe2f9d387c260af70d3b978da5a
Gourab Ghosh <ggourab217@gmail.com>
2026-02-01 22:30:35 +0530

    Update Crypto Trade Execution.c

--- Crypto Trade Execution.c
+++ Crypto Trade Execution.c
@@ -6,3 +6,5 @@ double currentPrice, targetPrice, walletBalance;
 int amount, volatility, hour;
 bool isWhale;
 printf("Enter currentPrice, targetPrice, balance, amount, volatility, hour, whale: ");
+  scanf("%lf %lf %lf %d %d %d %d", &currentPrice, &targetPrice,
+          &walletBalance, &amount, &volatility, &hour, &isWhale);
